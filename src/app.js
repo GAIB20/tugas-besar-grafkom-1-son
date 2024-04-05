445af299b8144db13ecba07f1defa8427b37a675
satrianababan <135211682std.stei.itb.ac.id>
2024-04-05 10:35:31 +0700

    feat : styling index.html

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+// canvas setup
+const canvas = document.getElementById('canvas');
+const gl = setupWebGL(canvas);
+const offsetCorr = (3.5 / 100) * window.innerHeight;    // corrections for css
+
+// list of [x, y] where -1 < x, y < 1
+let vertices = []; 
+
+// list of [r, g, b, a] where 0 < r, g, b, a < 1
+let colors = [];    
+let starting = [];
